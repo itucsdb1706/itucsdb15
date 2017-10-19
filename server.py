a57ab2fb4154cc99a15ba50379e13fa0d8b3b9d1
burakbugrul <bugrul@itu.edu.tr>
2017-10-20 02:01:18 +0300

    .gitignore & requirements.txt updated, added template(jinja) and view examples

--- server.py
+++ server.py
@@ -2,16 +2,30 @@ import datetime
 import os
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
+from flask.helpers import url_for
 
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+# TODO: Django like app partition
 
 
 @app.route('/')
 def home_page():
     now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
+    return render_template('home.html', current_time=now.ctime(), tmp_list=[1, 2, 3, 4], tmp_if=3)
+
+
+@app.route('/number_checker/<int:number>')
+def check_number(number):
+    """
+    If given number is less than 10, prints the number; else redirects to home page
+    """
+    if number >= 10:
+        return redirect(url_for('home_page'))
+    return render_template('number.html', number=number)
 
 
 if __name__ == '__main__':
