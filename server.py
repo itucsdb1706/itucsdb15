e5a6c761d73cd047ea6ee511b8837f31e03a71b5
Oguz Paksoy <oguz_paksoy_@hotmail.com>
2017-10-21 20:36:21 +0300

    Added ElephantSQL and Vagrant DSN support

--- server.py
+++ server.py
@@ -6,9 +6,23 @@ from num import num
 from user import get_user
 from problem import Problem
 
+import os
+import json
+import re
+
 
 lm = LoginManager()
 
+# ElephantSQL JSON parser for the DSN string
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @lm.user_loader
 def load_user(user_id):
@@ -29,6 +43,15 @@ def create_app():
 def main():
     app = create_app()
     port = app.config.get('PORT', 5000)
+
+    # Required code for ElephantSQL
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=5432 dbname='itucsdb'"""
+
     app.run(host='0.0.0.0', port=port)
 
 
