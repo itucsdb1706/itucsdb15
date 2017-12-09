1971e5a21096476674d838e4ffad6eadbca7d8c2
oguzp1 <oguz_paksoy_@hotmail.com>
2017-12-09 22:50:09 +0300

    Fixed injection issue, added drop

--- models/clarification.py
+++ models/clarification.py
@@ -3,22 +3,25 @@ from flask import current_app
 
 
 class Clarification:
-    fields = ['clarification_id', 'contest_id', 'user_id', 'clarification_content']
+    fields = ['clarification_id', 'contest_id', 'user_id', 'time_sent', 'clarification_content']
 
-    def __init__(self, contest_id=None, user_id=None, clarification_content=None):
-        self.clarification_id = None
+    def __init__(self, contest_id=None, user_id=None, time_sent=None, clarification_content=None,
+                 clarification_id=None):
         self.contest_id = contest_id
         self.user_id = user_id
+        self.time_sent = time_sent
         self.clarification_content = clarification_content
+        self.clarification_id = clarification_id
 
     def save(self):
         with dbapi2.connect(current_app.config['dsn']) as connection:
             cursor = connection.cursor()
-            statement = """INSERT INTO CLARIFICATION (contest_id, user_id, clarification_content) 
-                                  VALUES (%s, %s, %s) 
+            statement = """INSERT INTO CLARIFICATION (contest_id, user_id, time_sent,clarification_content) 
+                                  VALUES (%s, %s, %s, %s) 
                                   RETURNING clarification_id;"""
             cursor.execute(statement, (self.contest_id,
                                        self.user_id,
+                                       self.time_sent,
                                        self.clarification_content))
             self.clarification_id = cursor.fetchone()[0]
             cursor.close()
@@ -31,15 +34,14 @@ class Clarification:
             cursor.execute(statement, (self.clarification_id,))
             cursor.close()
 
-    def update(self):
+    def update_content(self):
         with dbapi2.connect(current_app.config['dsn']) as connection:
             cursor = connection.cursor()
             statement = """UPDATE CLARIFICATION
-                                  SET contest_id = %s, user_id = %s, clarification_content = %s
+                                  SET clarification_content = %s, time_sent = %s
                                   WHERE (clarification_id = %s);"""
-            cursor.execute(statement, (self.contest_id,
-                                       self.user_id,
-                                       self.clarification_content,
+            cursor.execute(statement, (self.clarification_content,
+                                       self.time_sent,
                                        self.clarification_id))
             cursor.close()
 
@@ -52,18 +54,19 @@ class Clarification:
                                   contest_id            INTEGER REFERENCES CONTEST(contest_id) ON DELETE CASCADE 
                                                         NOT NULL,
                                   user_id               INTEGER REFERENCES USERS(user_id) ON DELETE CASCADE NOT NULL,
-                                  clarification_content VARCHAR(512)
+                                  time_sent             TIMESTAMP NOT NULL,
+                                  clarification_content VARCHAR(2048) NOT NULL 
                                   );"""
             cursor.execute(statement)
             cursor.close()
 
     @staticmethod
-    def get(*args, **kwargs):
+    def get(**kwargs):
         with dbapi2.connect(current_app.config['dsn']) as connection:
             cursor = connection.cursor()
-            where_cond = ' '.join([key + ' = \'' + str(kwargs[key]) + '\'' for key in kwargs])
+            where_cond = ' AND '.join([key + ' = %s' for key in kwargs])
             statement = """SELECT * FROM CLARIFICATION WHERE (""" + where_cond + """);"""
-            cursor.execute(statement)
+            cursor.execute(statement, tuple(str(kwargs[key]) for key in kwargs))
             result = cursor.fetchall()
             cursor.close()
             return [Clarification.object_converter(item) for item in result]
@@ -76,7 +79,7 @@ class Clarification:
             cursor.execute(statement)
             result = cursor.fetchall()
             cursor.close()
-            return result
+            return [Clarification.object_converter(item) for item in result]
 
     @staticmethod
     def object_converter(values):
@@ -86,3 +89,11 @@ class Clarification:
             clarification.__setattr__(field, values[ind])
 
         return clarification
+
+    @staticmethod
+    def drop():
+        with dbapi2.connect(current_app.config['dsn']) as connection:
+            cursor = connection.cursor()
+            statement = """DROP TABLE CLARIFICATION;"""
+            cursor.execute(statement)
+            cursor.close()
